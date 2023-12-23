d842933c1d4dfb91f644c518487160056f724d6a
prajwalbundhade <prajwalpro231@gmail.com>
2023-12-23 23:09:57 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/zoyorooms";
+
+
+main ()
+    .then(() => {
+        console.log("MongoDB is connected");
+    })
+    .catch((err) => {
+        console.log(arr);
+    })
+async function main (){
+    await mongoose.connect(MONGO_URL);
+    
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+
+
+
+// index route
+app.get("/listings", async (req, res) => {
+    const allListings= await Listing.find({});
+    res.render("listings/index.ejs", {allListings})
+});
+
+// new route
+app.get("/listings/new", (req, res) => {
+   
+    res.render("listings/new.ejs")
+});
+
+// show route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+})
+
+//Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  });
+
+// Edit rout
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  });
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new listing",
+//         description: "my house",
+//         price: 1200,
+//         location: "Gadge Nagar, Amravati",
+//         country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample save ho gaya");
+//     res.send("successfull listing")
+// });
+
+
+app.listen(8080, ()=>  {
+    console.log("server is running at port 8080");
+})
